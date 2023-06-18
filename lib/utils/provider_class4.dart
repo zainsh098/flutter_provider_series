ce2aeeca979031afbcab89cd41862ce11ead9240
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-06-18 15:28:54 +0500

    File changes

---
+++ lib/utils/provider_class4.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProviderClass4 extends StatefulWidget {
+  const ProviderClass4({Key key}) : super(key: key);
+
+  @override
+  _ProviderClass4State createState() => _ProviderClass4State();
+}
+
+class _ProviderClass4State extends State<ProviderClass4> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
