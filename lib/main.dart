5efb8e00322580b6eb204af31fd8a799dec1b681
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2023-06-27 13:27:50 +0500

    Value Notifier Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_provider_series/provider/ExampleOne.dart';
+import 'package:flutter_provider_series/provider/auth_provider.dart';
+import 'package:flutter_provider_series/provider/count%20provider.dart';
+import 'package:flutter_provider_series/provider/favouriteExample.dart';
 import 'package:flutter_provider_series/provider/theme_mode.dart';
-import 'package:flutter_provider_series/screen/LightDarkMode.dart';
-import 'package:flutter_provider_series/screen/Notify.dart';
+
+import 'package:flutter_provider_series/screen/login.dart';
 import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
@@ -10,28 +13,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ThemeChanger(),
-      child: Builder(
-        builder: (BuildContext context) {
-          final themeChanger = Provider.of<ThemeChanger>(context);
+    return MultiProvider(providers: [
 
-          return MaterialApp(
-            title: 'Flutter Provider',
-            theme: ThemeData(
-              brightness: Brightness.light,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            themeMode: themeChanger.themeMode,
-            darkTheme: ThemeData(brightness: Brightness.dark,
-             appBarTheme: AppBarTheme(backgroundColor: Colors.amber)
+      ChangeNotifierProvider(create: (_) => CounterProvier()),
+      ChangeNotifierProvider(create: (_) => ExampleOneProvider()),
+      ChangeNotifierProvider(create: (_) => FavouriteExample()),
+      ChangeNotifierProvider(create: (_) => ThemeChanger()),
+      ChangeNotifierProvider(create: (_) => AuthProvider()),
 
-            ),
-            home: Notify(),
-          );
-        },
-      ),
+
+
+
+    ],
+    child: Builder(builder: (BuildContext context)
+
+    {
+      final themeChanger=Provider.of<ThemeChanger>(context);
+      return MaterialApp(
+      title: 'Flutter Provider',
+      theme: ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true,
+    ),
+    themeMode: themeChanger.themeMode,
+    darkTheme: ThemeData(brightness: Brightness.dark,
+    appBarTheme: AppBarTheme(backgroundColor: Colors.amber)
+
+    ),
+    home: Login(),
+    );
+
+
+    }
+
+    ),
+
+
+
+
     );
   }
 }
